c5737452c5abc5b54a63319bb9a2b447f71fe597
izun009 <izzudinwijaya19@gmail.com>
2024-01-15 15:20:05 +0700

    update deployment

---
+++ start.sh
@@ -0,0 +1,5 @@
+kubectl apply -f mongodb-secret.yaml
+kubectl apply -f mongodb-pvc.yaml
+kubectl apply -f mongodb-pv.yaml
+kubectl apply -f mongodb-service.yaml
+kubectl apply -f mongodb-deployment.yaml
